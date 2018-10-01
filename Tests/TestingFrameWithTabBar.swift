65260a4251d70b57b5ad36ef634cf1a9a291d514
devminjun <dev.mjun@gmail.com>
2018-10-01 22:52:24 +0900

    clear

--- Tests/TestingFrameWithTabBar.swift
+++ Tests/TestingFrameWithTabBar.swift
@@ -31,7 +31,6 @@ class TestingFrameWithTabBar: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
-        statusBarHeight = 0
         super.tearDown()
     }
     
