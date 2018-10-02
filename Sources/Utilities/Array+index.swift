d46a1b4e283cbf82933958199629730cd5d36bf4
devminjun <dev.mjun@gmail.com>
2018-10-02 19:17:48 +0900

    clear

--- Sources/Utilities/Array+index.swift
+++ Sources/Utilities/Array+index.swift
@@ -18,3 +18,6 @@ extension Array {
     }
 }
 
+
+
+
---
+++ Sources/Utilities/Then.swift
@@ -0,0 +1,9 @@
+//
+//  Then.swift
+//  Example
+//
+//  Created by minjuniMac on 02/10/2018.
+//  Copyright © 2018 mjun. All rights reserved.
+//
+
+import Foundation
